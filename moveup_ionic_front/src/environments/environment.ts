4e85ede0a09902cc78bad0216bed9ca7d053f5d6
Alberto Rugerio <alberto180310@gmail.com>
2026-03-25 02:00:55 -0600

    Quitar slash final de apiBaseUrl

--- moveup_ionic_front/src/environments/environment.ts
+++ moveup_ionic_front/src/environments/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://moveup-back.onrender.com/',
+  apiBaseUrl: 'https://moveup-back.onrender.com',
   apiKey: '288ad3d29b2949c49bb2af6901806a68',
 
   useMockApi: false,

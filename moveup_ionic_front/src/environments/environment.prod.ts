62b59e8e1da9df2ef5755de8302dfef3079ffbe5
Alberto Rugerio <alberto180310@gmail.com>
2026-03-25 01:45:07 -0600

    Actualizar environment.prod para backend en Render

--- moveup_ionic_front/src/environments/environment.prod.ts
+++ moveup_ionic_front/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://127.0.0.1:8000',
+  apiBaseUrl: 'https://moveup-back.onrender.com/',
   apiKey: '288ad3d29b2949c49bb2af6901806a68',
 
   useMockApi: false,
